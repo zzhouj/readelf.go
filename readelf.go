28c860050fcc7aec1fa1cd8e2690ec2441d13a29
justin <zzhouj@yahoo.com>
2018-04-04 16:55:52 +0800

    code refactor

--- readelf.go
+++ readelf.go
@@ -9,11 +9,11 @@ import (
 )
 
 var (
-	elfPath     string
-	showHeader  bool
-	showSection bool
-	showProgram bool
-	showAll     bool
+	elfPath       string
+	isShowHeader  bool
+	isShowSection bool
+	isShowProgram bool
+	isShowAll     bool
 )
 
 func init() {
@@ -27,16 +27,16 @@ func main() {
 		}
 		if strings.HasPrefix(arg, "-") { // options
 			if strings.Contains(arg, "a") {
-				showAll = true
+				isShowAll = true
 			}
 			if strings.Contains(arg, "h") {
-				showHeader = true
+				isShowHeader = true
 			}
 			if strings.Contains(arg, "S") {
-				showSection = true
+				isShowSection = true
 			}
 			if strings.Contains(arg, "S") {
-				showProgram = true
+				isShowProgram = true
 			}
 		} else { // elf path
 			elfPath = arg
@@ -50,44 +50,51 @@ func main() {
 		log.Fatal(err)
 	}
 	defer elfFile.Close()
-	if showHeader || showAll {
-		fmt.Printf("ELF File Header:\n")
-		fmt.Printf("  Class:      %s\n", elfFile.Class)
-		fmt.Printf("  Version:    %s\n", elfFile.Version)
-		fmt.Printf("  Data:       %s\n", elfFile.Data)
-		fmt.Printf("  OSABI:      %s\n", elfFile.OSABI)
-		fmt.Printf("  ABIVersion: %d\n", elfFile.ABIVersion)
-		fmt.Printf("  ByteOrder:  %s\n", elfFile.ByteOrder)
-		fmt.Printf("  Type:       %s\n", elfFile.Type)
-		fmt.Printf("  Machine:    %s\n", elfFile.Machine)
-		fmt.Printf("  Entry:      %d\n", elfFile.Entry)
+	if isShowHeader || isShowAll {
+		showHeader(elfFile)
 	}
-	if showSection || showAll {
-		sections := elfFile.Sections
-		fmt.Printf("ELF Sections:\n")
-		fmt.Printf("  [%2s] %-25s %-15s %-8s %-8s %-8s %2s %3s %3s %3s %3s\n", "Nr",
-			"Name",
-			"Type",
-			"Addr",
-			"Off",
-			"Size",
-			"ES",
-			"Flg",
-			"Lk",
-			"Inf",
-			"Al")
-		for i, section := range sections {
-			fmt.Printf("  [%2d] %-25s %-15s %08x %08x %08x %02x %3s %3d %3d %3d\n", i,
-				section.Name,
-				strings.Replace(section.Type.String(), "SHT_", "", -1),
-				section.Addr,
-				section.Offset,
-				section.Size,
-				section.Entsize,
-				strings.Replace(section.Flags.String(), "SHF_", "", -1),
-				section.Link,
-				section.Info,
-				section.Addralign)
-		}
+	if isShowSection || isShowAll {
+		showSections(elfFile.Sections)
+	}
+}
+
+func showHeader(file *elf.File) {
+	fmt.Printf("ELF File Header:\n")
+	fmt.Printf("  Class:      %s\n", file.Class)
+	fmt.Printf("  Version:    %s\n", file.Version)
+	fmt.Printf("  Data:       %s\n", file.Data)
+	fmt.Printf("  OSABI:      %s\n", file.OSABI)
+	fmt.Printf("  ABIVersion: %d\n", file.ABIVersion)
+	fmt.Printf("  ByteOrder:  %s\n", file.ByteOrder)
+	fmt.Printf("  Type:       %s\n", file.Type)
+	fmt.Printf("  Machine:    %s\n", file.Machine)
+	fmt.Printf("  Entry:      %d\n", file.Entry)
+}
+
+func showSections(sections []*elf.Section) {
+	fmt.Printf("ELF Sections:\n")
+	fmt.Printf("  [%2s] %-25s %-15s %-8s %-8s %-8s %2s %3s %3s %3s %3s\n", "Nr",
+		"Name",
+		"Type",
+		"Addr",
+		"Off",
+		"Size",
+		"ES",
+		"Flg",
+		"Lk",
+		"Inf",
+		"Al")
+	for i, section := range sections {
+		fmt.Printf("  [%2d] %-25s %-15s %08x %08x %08x %02x %3s %3d %3d %3d\n", i,
+			section.Name,
+			strings.Replace(section.Type.String(), "SHT_", "", -1),
+			section.Addr,
+			section.Offset,
+			section.Size,
+			section.Entsize,
+			strings.Replace(section.Flags.String(), "SHF_", "", -1),
+			section.Link,
+			section.Info,
+			section.Addralign)
 	}
 }
